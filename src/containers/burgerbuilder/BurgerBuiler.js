3a57afdb49f34c310dffe34c5d3c13fab236467c
Colin <beutt102@mail.chapman.edu>
2020-08-22 15:31:21 -0700

    ltupdate

--- src/containers/burgerbuilder/BurgerBuiler.js
+++ src/containers/burgerbuilder/BurgerBuiler.js
@@ -1,12 +1,24 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Auxcomp from '../../hoc/AuxComp'
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            meat: 2,
+            cheese: 2
+        }
+
+
+    }
+
     render() {
-        return(
+        return (
             <Auxcomp>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>
                     Burger Controls
                 </div>

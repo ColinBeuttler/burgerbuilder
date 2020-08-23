6cf08809af64b21c4d5b28b2681d7fdcf4fd5a05
Colin <beutt102@mail.chapman.edu>
2020-08-22 18:01:08 -0700

    updatefixed

--- src/containers/burgerbuilder/BurgerBuilder.js
+++ src/containers/burgerbuilder/BurgerBuilder.js
@@ -35,11 +35,25 @@ class BurgerBuilder extends Component {
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients}); 
     };
 
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients}); 
+    }
+
     render() {
         return (
             <Auxcomp>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls ingredientAdded={this.addedIngredientHandler}/>
+                <BuildControls ingredientAdded={this.addedIngredientHandler}
+                ingredientRemoved={this.removeIngredientHandler}/>
             </Auxcomp>
         )
     }

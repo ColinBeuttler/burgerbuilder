ab2c6376c3e4f8c4541bb3ef6c7801c2e6aae033
Colin Beuttler <beutt102@mail.chapman.edu>
2020-08-24 13:57:42 -0700

    dkupdate

--- src/containers/burgerbuilder/BurgerBuilder.js
+++ src/containers/burgerbuilder/BurgerBuilder.js
@@ -19,7 +19,22 @@ class BurgerBuilder extends Component {
             meat: 0,
             cheese: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable:false
+    }
+
+    updatePurchaseState (ingredients) {
+        // const ingredients = {
+        //     ...this.state.ingredients
+        // };
+        const sum = Object.keys(ingredients)
+        .map(igKey => {
+            return ingredients[igKey];
+        })
+        .reduce((sum, el ) => {
+            return sum + el;
+        }, 0 );
+        this.setState({purchasable: sum > 0})
     }
 
     addedIngredientHandler = (type) => {
@@ -33,6 +48,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+        this.updatePurchaseState(updatedIngredients);
     };
 
     removeIngredientHandler = (type) => {
@@ -49,6 +65,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
         this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+        this.updatePurchaseState(updatedIngredients);
     }
 
     render() {
@@ -66,7 +83,8 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addedIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo} 
-                    price={this.state.totalPrice}/>
+                    price={this.state.totalPrice}
+                    purchasable={this.state.purchasable}/>
             </Auxcomp>
         )
     }

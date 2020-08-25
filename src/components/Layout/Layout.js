cb81e8937bf495447e38f5a5e221c7ed1261b1da
Colin Beuttler <beutt102@mail.chapman.edu>
2020-08-24 17:24:07 -0700

    dkupdate

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Auxcomp from '../../hoc/AuxComp'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+import classes from './Layout.css'
 
 
 const layout = ( props ) =>(
  <Auxcomp>
+     <Toolbar />
      <div>
          Toolbar, Sidedrawer, Backdrop
      </div>
-     <main>
+     <main className={classes.Content}>
          {props.children}
      </main>
 
